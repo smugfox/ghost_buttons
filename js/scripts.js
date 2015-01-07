42042bf28749ff06c2f8b1d6fee3fef194c977b7
Robin Fox <robinfoxin@gmail.com>
2015-01-07 10:34:14 -0800

    Finished making ghost buttons

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$('.ghost_button.basic').click(function(){
+    $('.ghost_button_code_basic').toggle('slow');
+});
+
+$('.ghost_button.rounded').click(function(){
+    $('.ghost_button_code_rounded').toggle('slow');
+});
+
+$('.ghost_button.simpl_tr').click(function(){
+    $('.ghost_button_code_simpl_tr').toggle('slow');
+});
+
+$('.ghost_button.thick_bor').click(function(){
+    $('.ghost_button_code_thick_bor').toggle('slow');
+});
+
+$('.ghost_button.semi_tr').click(function(){
+    $('.ghost_button_code_semi_tr').toggle('slow');
+});
+
+$('.ghost_button.bc_fade').click(function(){
+    $('.ghost_button_code_bc_fade').toggle('slow');
+});
+
+$('.ghost_button.fc_fade').click(function(){
+    $('.ghost_button_code_fc_fade').toggle('slow');
+});
+
+$('.ghost_button.sz_tr').click(function(){
+    $('.ghost_button_code_sz_tr').toggle('slow');
+});
+
+
+
